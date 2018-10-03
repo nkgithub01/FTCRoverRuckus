be83d18a0a5d75074a4f3fbbb32910ffbd9caafb
nkgithub01 <32886053+nkgithub01@users.noreply.github.com>
2018-10-02 18:40:05 -0700

    drive stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Hardware;
 
 public class test extends OpMode{
 
@@ -9,8 +12,22 @@ public class test extends OpMode{
     /**********************************/
     ElapsedTime runtime = new ElapsedTime();
 
+    DcMotor leftBack = null;
+    DcMotor leftMiddle = null;
+    DcMotor leftFront = null;
+    DcMotor rightBack = null;
+    DcMotor rightMiddle = null;
+    DcMotor rightFront = null;
 
-    public void init() {}
+    public void init() {
+
+        DcMotor leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        DcMotor leftMiddle = hardwareMap.get(DcMotor.class, "leftMiddle");;
+        DcMotor leftFront = hardwareMap.get(DcMotor.class, "leftFront");;
+        DcMotor rightBack = hardwareMap.get(DcMotor.class, "rightBack");;
+        DcMotor rightMiddle = hardwareMap.get(DcMotor.class, "rightMiddle");;
+        DcMotor rightFront = hardwareMap.get(DcMotor.class, "rightFront");;
+    }
 
     public void start() { runtime.reset(); }
 
