6728d4ccd844bc2797f27c8b654489cfb32677a3
nkgithub01 <32886053+nkgithub01@users.noreply.github.com>
2018-10-04 16:41:42 -0700

    comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -1,5 +1,7 @@
+/*
+ * Code for user controlled part of the conpetition
+ */
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -13,6 +15,7 @@ public class test extends OpMode{
     /**********************************/
     ElapsedTime runtime = new ElapsedTime();
 
+    //Motors
     DcMotor leftBack = null;
     DcMotor leftMiddle = null;
     DcMotor leftFront = null;
@@ -20,8 +23,9 @@ public class test extends OpMode{
     DcMotor rightMiddle = null;
     DcMotor rightFront = null;
 
+    //Initialize the variables
     public void init() {
-
+        //Initialize the DcMotors
         DcMotor leftBack = hardwareMap.get(DcMotor.class, "leftBack");
         DcMotor leftMiddle = hardwareMap.get(DcMotor.class, "leftMiddle");;
         DcMotor leftFront = hardwareMap.get(DcMotor.class, "leftFront");;
@@ -29,6 +33,7 @@ public class test extends OpMode{
         DcMotor rightMiddle = hardwareMap.get(DcMotor.class, "rightMiddle");;
         DcMotor rightFront = hardwareMap.get(DcMotor.class, "rightFront");;
 
+        //Set the directions of the motors
         leftBack.setDirection(DcMotor.Direction.FORWARD);
         leftMiddle.setDirection(DcMotor.Direction.FORWARD);
         leftFront.setDirection(DcMotor.Direction.FORWARD);
@@ -36,15 +41,17 @@ public class test extends OpMode{
         rightMiddle.setDirection(DcMotor.Direction.REVERSE);
         rightFront.setDirection(DcMotor.Direction.REVERSE);
 
+        //Tell user that initialization is complete
         telemetry.addData("Status", "Initialized");
 
     }
 
-    public void start() { runtime.reset(); }
+    public void start() {
+        runtime.reset();
+    }
 
     public void loop() {
         // drive the robot
-
         leftBack.setPower(gamepad1.left_stick_y);
         leftMiddle.setPower(gamepad1.left_stick_y);
         leftFront.setPower(gamepad1.left_stick_y);
@@ -52,11 +59,7 @@ public class test extends OpMode{
         rightMiddle.setPower(gamepad1.right_stick_y);
         rightFront.setPower(gamepad1.right_stick_y);
 
-
-
-
-
-
+        //Displays the runtime
         telemetry.addData("Runtime: ", getRuntime());
     }
 
