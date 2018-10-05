b0cbb78afad30f6213207e5b75933eb3378ca4ae
nkgithub01 <32886053+nkgithub01@users.noreply.github.com>
2018-10-04 21:14:47 -0700

    required
    you must specify whether this a code for tele-op or autonomous at the beginning of the class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -3,12 +3,14 @@
  */
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Hardware;
 
+@TeleOp (name = "testDrive")
 public class test extends OpMode{
 
     /* objects*/
