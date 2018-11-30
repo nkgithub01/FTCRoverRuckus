3cdbc9dc9458736913c969ba549fc2d12cb5a29f
Kevin Zhao <kevinzhao.basis@gmail.com>
2018-11-29 16:59:50 -0800

    basic auton functions and comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousShort.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousShort.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -6,7 +7,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="AutonomousShort", group="OpMode")
-public class AutonomousShort extends OpMode{
+public class AutonomousShort extends LinearOpMode{
 
     //Objects
     /**********************************/
@@ -26,9 +27,12 @@ public class AutonomousShort extends OpMode{
     boolean inForward = false;
     boolean inBackward = false;
     boolean ab2Pressed = false;
-    
+
+    final double ticksPerInch = 1;
+    final double ticksPerDegree = 1;
+
     //Initialize the variables
-    public void init() {
+    public void runOpMode() {
         //Initialize the DcMotors
         leftBack = hardwareMap.get(DcMotor.class, "leftBack");
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
@@ -60,6 +64,15 @@ public class AutonomousShort extends OpMode{
         rightLin.setDirection(DcMotor.Direction.REVERSE);
         leftLin.setDirection(DcMotor.Direction.FORWARD);
 
+        //Set the directions of the motors
+        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftLin.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightLin.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
         //Set the direction of the servo
         intake.setDirection(Servo.Direction.FORWARD);
 
@@ -71,17 +84,54 @@ public class AutonomousShort extends OpMode{
 
         intake.setPosition(0);
 
+        waitForStart();
+
+        run(5);
     }
 
-    public void start() {
-        runtime.reset();
+    /**
+     * Rotates right by a number of degrees
+     * @param degrees the number of degrees to rotate right
+     */
+    public void rotateRight(double degrees) {
+        int deg = (int) (degrees * ticksPerDegree);
+        leftBack.setTargetPosition(leftBack.getTargetPosition() + deg);
+        leftFront.setTargetPosition(leftBack.getTargetPosition() + deg);
+        rightBack.setTargetPosition(leftBack.getTargetPosition() + deg);
+        rightFront.setTargetPosition(leftBack.getTargetPosition() + deg);
+        setPowerAllMotors(100);
     }
 
-    public void loop() {
+    /**
+     * Go forward a number of inches
+     * @param inches the distance to travel
+     */
+    public void run(double inches) {
+        setAllMotors((int) (inches * ticksPerInch));
+        setPowerAllMotors(100);
+    }
 
+    /**
+     * Set how much to go forward on all motors
+     * @param extraPos the amount of encoder ticks
+     */
+    public void setAllMotors(int extraPos) {
+        leftBack.setTargetPosition(leftBack.getTargetPosition() + extraPos);
+        rightBack.setTargetPosition(leftBack.getTargetPosition() - extraPos);
+        leftFront.setTargetPosition(leftBack.getTargetPosition() + extraPos);
+        rightFront.setTargetPosition(leftBack.getTargetPosition() - extraPos);
+    }
 
-        //Displays the runtime
-        telemetry.addData("Runtime: ", getRuntime());
+    /**
+     * Set power to all motors
+     * @param power power to be set for all motors, must be between -100 and 100
+     */
+    public void setPowerAllMotors(int power) {
+        if (Math.abs(power) > 100) return;
+        leftBack.setPower(power);
+        rightBack.setPower(power);
+        leftFront.setPower(power);
+        rightFront.setPower(power);
     }
 
 }
