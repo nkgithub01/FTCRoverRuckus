08016557f8062911c9faaceb50d27dba7afcd678
Kevin Zhao <kevinzhao.basis@gmail.com>
2019-01-17 19:54:32 -0800

    change name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FourWheelDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FourWheelDrive.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 /*
     Created by me ON 10/23/18
  */
-@TeleOp(name="4wheelD", group="OpMode")
+@TeleOp(name="FourWheelDrive", group="OpMode")
 public class FourWheelDrive extends OpMode{
 
     /* objects*/
